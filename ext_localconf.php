30352b20bcea9bf5888e5606c858ddb4f8df6241
Stefan Froemken <froemken@gmail.com>
2020-11-03 14:09:27 +0100

    Add needed extension files like gitignore and editorconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,10 @@
 <?php
-
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['contributory_calculator'] = JWeiland\ContributoryCalculator\Hook\ProcessDatamap::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['contributory_calculator']
+    = JWeiland\ContributoryCalculator\Hook\ProcessDatamap::class;
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'JWeiland.ContributoryCalculator',
