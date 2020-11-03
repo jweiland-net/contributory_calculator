89c1beff7bd8afea4a284b9a95e4f454f1f6209e
Stefan Froemken <froemken@gmail.com>
2020-11-03 14:44:29 +0100

    Add new icons and register plugin for newContentElementWizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,37 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['contributory_calculator']
-    = JWeiland\ContributoryCalculator\Hook\ProcessDatamap::class;
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'JWeiland.ContributoryCalculator',
+        'Contributorycalculator',
+        [
+            'Search' => 'search, result',
+        ],
+        // non-cacheable actions
+        [
+            'Search' => 'search, result',
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'JWeiland.ContributoryCalculator',
-    'Contributorycalculator',
-    [
-        'Search' => 'search, result',
-    ],
-    // non-cacheable actions
-    [
-        'Search' => 'search, result',
-    ]
-);
+    // add calculator plugin to new element wizard
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:contributory_calculator/Configuration/TSconfig/ContentElementWizard.txt">'
+    );
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['contributory_calculator']
+        = JWeiland\ContributoryCalculator\Hook\ProcessDatamap::class;
+
+    // Register SVG Icon Identifier
+    $svgIcons = [
+        'ext-contributorycalculator-wizard-icon' => 'plugin_wizard.svg',
+    ];
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    foreach ($svgIcons as $identifier => $fileName) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:contributory_calculator/Resources/Public/Icons/' . $fileName]
+        );
+    }
+});
