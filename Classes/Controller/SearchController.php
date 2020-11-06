c218cf6fdf3c6248c575d4c8c458c6a6d8ef52c2
Stefan Froemken <froemken@gmail.com>
2020-11-06 12:16:34 +0100

    Add possibility to set min/max income in plugin

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace JWeiland\ContributoryCalculator\Controller;
 
+use JWeiland\ContributoryCalculator\Domain\Factory\SearchFactory;
 use JWeiland\ContributoryCalculator\Domain\Model\Search;
 use JWeiland\ContributoryCalculator\Domain\Repository\CareRepository;
 use JWeiland\ContributoryCalculator\Service\Calculator;
@@ -30,15 +31,21 @@ class SearchController extends ActionController
      */
     protected $careRepository;
 
-    public function __construct(CareRepository $chargeableIncomeRepository)
+    /**
+     * @var SearchFactory
+     */
+    protected $searchFactory;
+
+    public function __construct(CareRepository $chargeableIncomeRepository, SearchFactory $searchFactory)
     {
         $this->careRepository = $chargeableIncomeRepository;
+        $this->searchFactory = $searchFactory;
     }
 
     public function searchAction(): void
     {
         $this->view->assign('careForms', $this->careRepository->findAll());
-        $this->view->assign('search', GeneralUtility::makeInstance(Search::class));
+        $this->view->assign('search', $this->searchFactory->getSearch());
     }
 
     /**
@@ -46,9 +53,12 @@ class SearchController extends ActionController
      */
     public function resultAction(Search $search): void
     {
+        $search = $this->searchFactory->getSearch($search);
         $calculator = $this->objectManager->get(Calculator::class);
+
         $this->view->assign('careForms', $this->careRepository->findAll());
         $this->view->assign('search', $search);
+
         try {
             $this->view->assign('result', $calculator->getTotalPerMonth($search));
         } catch (EmptyFactorException $e) {
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,13 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['contributorycalculator_contributorycalculator'] = 'select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['contributorycalculator_contributorycalculator'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'contributorycalculator_contributorycalculator',
+    'FILE:EXT:contributory_calculator/Configuration/FlexForms/ContributoryCalculator.xml'
+);
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'JWeiland.ContributoryCalculator',
     'Contributorycalculator',
