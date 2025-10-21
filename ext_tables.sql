a54ecf405dd6b2f03adea594f6f16fec9a608d38
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-10-21 14:02:53 +0200

    [TASK] Updated ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,10 +12,10 @@ CREATE TABLE tx_contributorycalculator_domain_model_care
 #
 CREATE TABLE tx_contributorycalculator_domain_model_calculationbase
 (
-	year_of_validity         int   DEFAULT 0   NOT NULL,
-	value_below_3            float DEFAULT '0' NOT NULL,
-	value_above_3            float DEFAULT '0' NOT NULL,
-	care_form                int   DEFAULT 0   NOT NULL,
-	minimal_income int   DEFAULT 0   NOT NULL,
-	maximum_income int   DEFAULT 0   NOT NULL
+	year_of_validity int   DEFAULT 0   NOT NULL,
+	value_below_3    float DEFAULT '0' NOT NULL,
+	value_above_3    float DEFAULT '0' NOT NULL,
+	care_form        int   DEFAULT 0   NOT NULL,
+	minimal_income   int   DEFAULT 0   NOT NULL,
+	maximum_income   int   DEFAULT 0   NOT NULL
 );
