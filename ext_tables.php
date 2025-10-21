082a42a53b0e8577ae6f95fbc6afa8b17b3bc16f
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-10-21 14:03:59 +0200

    [TASK] Removed ext_tables.php file

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_contributorycalculator_domain_model_care',
-    'EXT:contributory_calculator/Resources/Private/Language/locallang_csh_tx_contributorycalculator_domain_model_care.xlf'
-);
