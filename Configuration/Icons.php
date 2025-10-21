577e0387e94d848dab1890b34e460a0db9b8362e
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-10-21 14:00:45 +0200

    [TASK] Moded Icon registrations to Configuration/Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'ext-contributorycalculator-wizard-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:contributory_calculator/Resources/Public/Icons/plugin_wizard.svg'
+    ],
+];
